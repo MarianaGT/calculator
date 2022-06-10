717a0ee9c8e157e35a4f5bde534adf22f56ecbcf
Enrique Ramirez <nrqrmz@gmail.com>
2022-06-10 05:55:13 -0500

    Update formulas for pescados_y_mariscos

--- app/controllers/cuestionarios_controller.rb
+++ app/controllers/cuestionarios_controller.rb
@@ -135,21 +135,62 @@ class CuestionariosController < ApplicationController
 
   # THIS SECTION CONTAINS LOGIC AND FORMULAS FOR CALORIES CALCULATOR
 
+  def obtener_calorias(kcal_porcion, frecuencia_consumo)
+    (kcal_porcion * frecuencia_consumo / 7).round(2)
+  end
+
+  def obtener_calorias_pescados_y_mariscos(grupo, frecuencia_consumo)
+    calorias_segun_grupo = [226, 195, 194]
+    kcal_porcion = calorias_segun_grupo[grupo.to_i]
+    (kcal_porcion * frecuencia_consumo / 7).round(2)
+  end
+
   def obtener_carbono(origen, arr_valores_kg_co2, porciones_por_kilo, frecuencia_consumo)
     origen_alimento = ['no consumo', 'organico', 'intensivo'].index(origen)
     kg_co2 = arr_valores_kg_co2[origen_alimento]
     (kg_co2 / porciones_por_kilo * frecuencia_consumo / 7).round(2)
   end
 
-  def obtener_calorias(kcal_porcion, frecuencia_consumo)
-    (kcal_porcion * frecuencia_consumo / 7).round(2)
+  def obtener_carbono_pescados_y_mariscos(origen, grupo, frecuencia_consumo)
+    origen_alimento = ['no consumo', 'organico', 'intensivo'].index(origen)
+    valores_grupos_carbono = [[2.95, 3.71, 3.71],
+                              [9.42, 10.13, 10.13],
+                              [0.73, 1.44, 1.44]]
+    valores_grupos_porciones = [5, 6.25, 5]
+    arr_valores_kg_co2 = valores_grupos_carbono[grupo.to_i]
+    porciones_por_kilo = valores_grupos_porciones[grupo.to_i]
+    kg_co2 = arr_valores_kg_co2[origen_alimento]
+    (kg_co2 / porciones_por_kilo * frecuencia_consumo / 7).round(2)
   end
 
   def kcal_calculator
-    @calorias = obtener_calorias(135, @cuestionario.frecuencia_res)
+    calorias_res = obtener_calorias(135, @cuestionario.frecuencia_res)
+    calorias_cerdo = obtener_calorias(297, @cuestionario.frecuencia_puerco)
+    calorias_borrego = obtener_calorias(177, @cuestionario.frecuencia_borrego)
+    calorias_pollo = obtener_calorias(195, @cuestionario.frecuencia_pollo)
+    calorias_leche = obtener_calorias(152, @cuestionario.frecuencia_leche)
+    calorias_queso = obtener_calorias(100, @cuestionario.frecuencia_queso)
+    calorias_yogurt = obtener_calorias(91, @cuestionario.frecuencia_yogurt)
+    calorias_pescados_y_mariscos = obtener_calorias_pescados_y_mariscos(@cuestionario.valores_pescados_mariscos, @cuestionario.frecuencia_pescados_mariscos)
+
+    @calorias = [calorias_res, calorias_cerdo, calorias_borrego, calorias_pollo, calorias_leche, calorias_queso, calorias_yogurt, calorias_pescados_y_mariscos].sum
   end
 
   def huella_carbono
-    @carbono = obtener_carbono(@cuestionario.origen_carne, [16.33,	16.33, 26.99], 10, @cuestionario.frecuencia_res )
+    carbono_res = obtener_carbono(@cuestionario.origen_carne, [16.33,	16.33, 26.99], 10, @cuestionario.frecuencia_res)
+    carbono_puerco = obtener_carbono(@cuestionario.origen_carne, [4.5, 4.5, 6.17], 10, @cuestionario.frecuencia_puerco)
+    carbono_borrego = obtener_carbono(@cuestionario.origen_carne, [24.34, 24.34, 25.09], 10, @cuestionario.frecuencia_borrego)
+    carbono_pollo = obtener_carbono(@cuestionario.origen_carne, [3.1, 3.1, 3.65], 10, @cuestionario.frecuencia_pollo)
+    carbono_leche = obtener_carbono(@cuestionario.origen_leche, [1.14, 1.14, 1.32], 4, @cuestionario.frecuencia_leche)
+    carbono_queso = obtener_carbono(@cuestionario.origen_leche, [9.31, 10.61, 10.61], 33.33, @cuestionario.frecuencia_queso)
+    carbono_yogurt = obtener_carbono(@cuestionario.origen_leche, [0.79, 0.9, 0.9], 8, @cuestionario.frecuencia_yogurt)
+    carbono_pescados_y_mariscos = obtener_carbono_pescados_y_mariscos(@cuestionario.origen_carne, @cuestionario.valores_pescados_mariscos, @cuestionario.frecuencia_pescados_mariscos)
+
+    @carbono = [carbono_res, carbono_puerco, carbono_borrego, carbono_pollo, carbono_leche, carbono_queso, carbono_yogurt, carbono_pescados_y_mariscos].sum
   end
+
+  # no hay valores para salmon, atun, que estan en el cuestionario
+  # en el cuestionario no hay preguntas de pavo pero si estan los valores
+  # no hay pregunta del origen del queso ni del yogurt, calculado con el origen de la leche
+  # no hay pregunta del origen del pescado, calculado con el origen de la carne
 end
