bf99a1f15aa558a6704a634e99b0a595827a5dbc
ErnestG <egonzl92@outlook.com>
2022-09-10 21:36:11 -0700

    fixes validations
    removes usless tag

---
+++ config/importmap.rb
@@ -0,0 +1,4 @@
+
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,11 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
 
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $:'jquery/src/jquery',
+        JQuery: 'jquery/src/jquery'
+    }))
 module.exports = environment
