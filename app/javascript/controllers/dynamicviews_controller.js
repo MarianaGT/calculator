afee13a443baf9f728d29120135b2429fdc4f5a9
ErnestG <egonzl92@outlook.com>
2022-09-16 11:34:53 -0700

    more dynamics controls
    - Tortillas
    - Tacos
    - Pan
    - Bolillos
    - Embutidos

--- app/javascript/controllers/dynamicviews_controller.js
+++ app/javascript/controllers/dynamicviews_controller.js
@@ -2,12 +2,43 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dynamicviews"
 export default class extends Controller {
-  static targets = ["fishfrequency", "fishgroup","eggfrequency", "eggoutput","eggamount","vegfrequency", "vegoutput", "vegamount"]
+  static targets = [
+      "fishfrequency",
+    "fishgroup",
+    "eggfrequency",
+    "eggoutput",
+    "eggamount",
+    "vegfrequency",
+    "vegoutput",
+    "vegamount",
+    "fruitfrequency",
+    "fruitoutput",
+    "fruitamount",
+    "tortillafrequency",
+    "tortillaoutput",
+    "tortillaamount",
+    "tacofrequency",
+    "tacooutput",
+    "bolillofrequency",
+    "bolillooutput",
+    "bolilloamount",
+    "panfrequency",
+    "panoutput",
+    "panamount",
+    "embutidosfrequency",
+    "embutidosoutput"
+  ]
   static values = {hideelement: Number}
   connect() {
     this.toggle()
     this.showEggAmountControl()
     this.showVeggies()
+    this.showFruit()
+    this.showTortillas()
+    this.showBolillos()
+    this.showPan()
+    this.showEmbutidos(),
+    this.showTaco()
   }
   toggle(e) {
       if(e!==undefined) {
@@ -46,7 +77,88 @@ export default class extends Controller {
       this.vegamountTarget.value = 0
     }
   }
+  showFruit(e) {
+    if(e !== undefined) {
+      if(this.checkForHidingElement(e.target.value, this.hideelementValue)) {
+        this.fruitoutputTarget.hidden = true
+        this.fruitamountTarget.value = 0
+      } else {
+        this.fruitoutputTarget.hidden = false
+      }
+    } else {
+      this.fruitoutputTarget.hidden = true
+      this.fruitamountTarget.value = 0
+    }
+  }
+  showTortillas(e){
+    if(e !== undefined) {
+      if(this.checkForHidingElement(e.target.value, this.hideelementValue)) {
+        this.tortillaoutputTarget.hidden = true
+        this.tortillaamountTarget.value = 0
+      } else {
+        this.tortillaoutputTarget.hidden = false
+      }
+    } else {
+      this.tortillaoutputTarget.hidden = true
+      this.tortillaamountTarget.value = 0
+    }
+
+  }
   checkForHidingElement(el, ref) {
     return el == ref
+  }
+
+  showBolillos(e) {
+    if(e !== undefined) {
+      if(this.checkForHidingElement(e.target.value, this.hideelementValue)) {
+        this.bolillooutputTarget.hidden = true
+        this.bolilloamountTarget.value = 0
+      } else {
+        this.bolillooutputTarget.hidden = false
+      }
+    } else {
+      this.bolillooutputTarget.hidden = true
+      this.bolilloamountTarget.value = 0
+    }
+
+  }
+
+  showPan(e) {
+    if(e !== undefined) {
+      if(this.checkForHidingElement(e.target.value, this.hideelementValue)) {
+        this.panoutputTarget.hidden = true
+        this.panamountTarget.value = 0
+      } else {
+        this.panoutputTarget.hidden = false
+      }
+    } else {
+      this.panoutputTarget.hidden = true
+      this.panamountTarget.value = 0
+    }
+  }
+
+  showEmbutidos(e) {
+    if(e !== undefined) {
+      if(this.checkForHidingElement(e.target.value, this.hideelementValue)) {
+        this.embutidosoutputTarget.hidden = true
+      } else {
+        this.embutidosoutputTarget.hidden = false
+      }
+    } else {
+      this.embutidosoutputTarget.hidden = true
+    }
+  }
+
+  showTaco(e) {
+    if(e !== undefined) {
+      if(this.checkForHidingElement(e.target.value, this.hideelementValue)) {
+        this.tacooutputTarget.hidden = true
+      } else {
+        this.tacooutputTarget.hidden = false
+      }
+    } else {
+      this.tacooutputTarget.hidden = true
+    }
+
   }
 }
